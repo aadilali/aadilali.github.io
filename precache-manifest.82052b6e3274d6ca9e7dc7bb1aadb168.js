0dba222175380bc1135fbb82ab04cd7e7957df0d
Adil Ali <adil.ali@nextbridge.org>
2020-10-14 21:47:41 +0500

    Updates

---
+++ precache-manifest.82052b6e3274d6ca9e7dc7bb1aadb168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16f5a42c42f3215b2c83cc8e40f66a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd6981144acb800ec61d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d0fd81e5dfd961760b1b",
+    "url": "/static/js/2.3d72c43c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3d72c43c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6981144acb800ec61d",
+    "url": "/static/js/main.49e0531f.chunk.js"
+  },
+  {
+    "revision": "6bfefcbe83360b33814b",
+    "url": "/static/js/runtime-main.b13acbbd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
